206b93dc4314f368bc1fa055e8702f9242146bc3
mkgh25 <silverangel6211@gmail.com>
2025-04-19 01:20:53 +0000

    updated whole main

--- shoppinglist/Program.cs
+++ shoppinglist/Program.cs
@@ -19,11 +19,17 @@ class Program
         public string RecipeName { get; set;}
         public List<string> Ingredients {get; set;}//creates a property that can be retrieved and set (value assigned)
     }
+    class MealPlan//create class of mealsplan (description of what a mealplan is)
+    {
+        public string MealTime { get; set;}//which mealtime is this?
+        public DateTime Date { get; set;}//date for meal plan created for
+        public RecipeClass RecipeName { get; set;}//Recipe name from Recipe Class
+        public int MealNumber {get; set;}//what meal number
+    }
     //create the source of the data I want to import/use in program.
     static string foodfilepath="food-database.txt";
     static string recipefilepath="recipe-database.txt";
-    
-    
+    static string mealsfilepath="meals-database.txt";
 
     //creates a placeholder list for the data to be retrieved for other functions to be used.
     static List<FoodItem> LoadFoodItemList()
@@ -64,7 +70,43 @@ class Program
         }
         return recipes;
     }
-
+    static List<MealPlan> LoadMealPlanList()
+    {
+        List <MealPlan> meals = new List<MealPlan>();
+        string[] lines = File.ReadAllLines(mealsfilepath);
+        foreach (string line in lines)
+        {
+            var mealparts =line.Split(",");
+            if (mealparts.Length == 4)
+            {
+                string datestr = mealparts[0].Trim();
+                string mealtime = mealparts[1].Trim();
+                string strmealnum = mealparts[2].Trim();
+                string recipeName = mealparts[3].Trim();
+                //DateTime date = DateTime.Parse(datestr);//need to parse date and mealnumber;
+                //int mealnumber = int.Parse(strmealnum);
+                if (!DateTime.TryParse(datestr, out DateTime date))//parse date for storing and validate format.
+                {
+                    Console.WriteLine($"Invalid date.");
+                    continue;
+                }
+                if(!int.TryParse(strmealnum, out int mealnumber))
+                {
+                    Console.WriteLine($"invalid mealnumber");
+                    continue;
+                }
+                {meals.Add(new MealPlan
+                    {
+                        Date = date,
+                        MealTime = mealtime,
+                        MealNumber = mealnumber,
+                        RecipeName = new RecipeClass {RecipeName = recipeName}
+                    });
+                }
+            }
+        }
+        return meals;
+    }
     //create method to save fooditems lists and recipe lists to .txt files.
     static void SaveFoodItemList(List<FoodItem> items)
     {
@@ -87,6 +129,16 @@ class Program
         File.WriteAllLines(recipefilepath,lines);
     }
 
+    static void SaveMealPlanList(List<MealPlan> meals)
+    {
+        List<string> lines = new List<string>();
+        foreach (var meal in meals)
+        {
+            string line = $"{meal.Date:yyyy-MM-dd},{meal.MealTime},{meal.MealNumber},{meal.RecipeName.RecipeName}";
+            lines.Add(line);
+        }
+        File.WriteAllLines(mealsfilepath, lines);
+    }
 
 
     static void Main(string[] args)
@@ -99,6 +151,10 @@ class Program
         {
             File.Create(recipefilepath).Close();
         }
+        if (!File.Exists(mealsfilepath))
+        {
+            File.Create(mealsfilepath).Close();
+        }
 
         //Call lists from methods
         List<FoodItem> items = LoadFoodItemList();
@@ -108,6 +164,7 @@ class Program
         string usercommand;
         string fcommand;
         string rcommand;
+        string mcommand;
 
 
         do {
@@ -119,14 +176,15 @@ class Program
             Console.WriteLine("report = to display shopping list");
             Console.WriteLine("exit = to exit program");
             usercommand = Console.ReadLine();
+            //Console.WriteLine($"DEBUG: You typed '{usercommand}'");
 
-        //Shopping list 
+            //Shopping list - module
         
             if(usercommand == "shoppinglist")
             {
         
                 do{
-                    Console.WriteLine("Select one: add = (Add Food Item), remove = (Remove Food item), menu (go back to main menu)");
+                    Console.WriteLine("Select one: add = (Add Food Item), update = (update food item status), remove = (Remove Food item), menu (go back to main menu)");
                     fcommand=Console.ReadLine();
                     string item;
                     string grocerycategory;
@@ -147,6 +205,41 @@ class Program
                         items.Add(new FoodItem {GroceryCategory = grocerycategory, FoodName=item, Status=status});
                         SaveFoodItemList(items);
                     }while (true);
+                    else if (fcommand == "update")
+                    {
+                        Console.WriteLine("Enter Food name to update:");
+                        string itemtoupdate = Console.ReadLine().Trim().ToLower();
+
+                        FoodItem match = null;
+                        foreach(var i in items)
+                        {
+                            if (i.FoodName.ToLower() == itemtoupdate)
+                            {
+                                match = i;
+                                break;
+                            }
+                        }
+                        if (match != null)
+                        {
+                            Console.WriteLine($"Current status is: {match.Status}");
+                            Console.WriteLine("Enter new status (needed or have): ");
+                            string newStatus = Console.ReadLine().Trim().ToLower();
+                            if (newStatus == "needed" || newStatus=="have")
+                            {
+                                match.Status=newStatus;
+                                SaveFoodItemList(items);
+                                Console.WriteLine("Food item status updated.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid status.Please enter 'needed' or 'have'.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Food item not found.");
+                        }
+                    }
                     else if (fcommand == "remove")
                     {
                         Console.Write("Enter a food item name to remove:");
@@ -179,7 +272,7 @@ class Program
                 }while(fcommand!="menu");  
             }    
             else if (usercommand=="recipes")
-            {//Recipes
+            {//Recipes - Module
                 do{
                     Console.WriteLine("Select one: add (to add a recipe), remove (to remove a recipe), menu (To go back to main menu)");
                     rcommand=Console.ReadLine()?.Trim().ToLower();
@@ -207,6 +300,31 @@ class Program
                         rlist.Add(newRecipe);//adding new recipe and ingredients to recipe list
                         SaveRecipeList(rlist);
                         Console.WriteLine("new recipe added");
+
+                    //check to see if ingredient is in is in shoppinglist
+                        foreach (var ingredient in newRecipe.Ingredients)
+                        {
+                            if(!items.Any(i => i.FoodName.Equals(ingredient, StringComparison.OrdinalIgnoreCase)))
+                            {
+                                Console.WriteLine($"Ingredient'{ingredient}' not found in shoppinglist");
+                                Console.Write("Enter grocery category for this item: ");
+                                string category = Console.ReadLine().Trim().ToLower();
+                                if (string.IsNullOrEmpty(category))
+                                {
+                                    category = "misc";
+                                }
+                                items.Add(new FoodItem
+                                {
+                                    GroceryCategory = category,
+                                    FoodName = ingredient,
+                                    Status = "needed"
+                                });
+                            }
+
+                        }
+                        SaveFoodItemList(items); //save added ingredients to shopping list
+                        Console.WriteLine("ingredients added to shopping list");
+
                     }
                     else if (rcommand=="remove")
                     {
@@ -226,7 +344,7 @@ class Program
                                 break;
                             }  
                         }   
-                        if(recipetoremove != null)
+                        if(recipeToRemove != null)
                         {
                             rlist.Remove(recipeToRemove);
                             SaveRecipeList(rlist);
@@ -239,34 +357,138 @@ class Program
                     }
                 } while(rcommand !="menu");         
             } 
-            else if (usercommand=="mealplan")
-            {//mealplan
-              Console.WriteLine("under development");        
-            } 
-            else if (usercommand=="report")
-            {//Report
-              Console.WriteLine("Shopping List Report => Needed Items by Grocery Category");
-              string [] shoplistreport = File.ReadAllLines(foodfilepath); 
-              foreach (string line in shoplistreport)
-              {
-                string [] foodparts = line.Split(",");
-                if(foodparts.Length == 3)
-                {
-                    string GroceryCategory = foodparts[0].Trim();
-                    string foodName = foodparts[1];
-                    string status = foodparts[2];
-                    if (status == "needed")
-                    {
-                    Console.WriteLine($"- {GroceryCategory}: {foodName}");
-                    }
-                }
-                else
-                {
-                  Console.WriteLine("Error");  
-                }
-              }       
-            } 
+            else if (usercommand=="mealplan") 
+            {//mealplan - module
+              //Console.WriteLine("under development"); 
+                List<MealPlan> mealList = LoadMealPlanList();//use current meal plan list from method
+                do{
+                    Console.WriteLine("Select one: add (add a mealplan), remove (remove a mealplan), menu (To go back to main menu)");
+                    mcommand = Console.ReadLine().Trim().ToLower();
 
-        }while(usercommand !="exit");
-    }
+                    if (mcommand=="add"){
+                        Console.WriteLine("Enter Mealplan start date: yyyy-MM-dd");
+                        DateTime date = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter mealtime selection: Breakfast, Lunch, or Dinner");
+                        string mealTime = Console.ReadLine().Trim().ToLower();
+                        Console.WriteLine("Enter recipe name for this meal:");
+                        String recipeName = Console.ReadLine().Trim().ToLower();
+                        int mealnumber = mealList.Count + 1;
+
+                        RecipeClass selectRecipe = null;
+                        foreach(var recipe in rlist)
+                        {
+                            if (recipe.RecipeName.Equals(recipeName, StringComparison.OrdinalIgnoreCase))
+                            {
+                                selectRecipe = recipe;//find recipe in list;
+                                break;
+                            }
+                        }
+                        if(selectRecipe != null)
+                        {
+                        mealList.Add(new MealPlan
+                        {
+                            Date = date,
+                            MealTime = mealTime,
+                            MealNumber = mealnumber,
+                            RecipeName = new RecipeClass{RecipeName = selectRecipe.RecipeName}
+                        });
+                        SaveMealPlanList(mealList);
+                        Console.WriteLine("Meal plan saved");                        
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No recipe match found, please go to recipes and add to recipe list");
+                        }
+                    } 
+                    else if (mcommand == "remove")
+                        {
+                        Console.WriteLine("Enter the date of meal plan you want to remove: yyyy-mm-dd");
+                        string dateinput = Console.ReadLine();
+                        DateTime removedate;
+                        if (DateTime.TryParse(dateinput, out removedate))
+                        {
+                        mealList.RemoveAll(m => m.Date.Date == removedate.Date);
+                        SaveMealPlanList(mealList);
+                        Console.WriteLine("Meal plan(s) removed for " + removedate.ToString("yyyy-MM-dd"));
+                        }
+                        else {
+                        Console.WriteLine("no mealplans found");
+                        }
+                    };
+                }while(mcommand != "menu");
+            } 
+            else if (usercommand == "report")
+                {
+                Console.WriteLine("Shopping List Report => Needed Items by Grocery Category");
+                string [] shoplistreport = File.ReadAllLines(foodfilepath); 
+                foreach (string line in shoplistreport)
+                {
+                    string [] foodparts = line.Split(",");
+                    if(foodparts.Length == 3)
+                    {
+                        string GroceryCategory = foodparts[0].Trim();
+                        string foodName = foodparts[1];
+                        string status = foodparts[2];
+                        if (status == "needed")
+                        {
+                            Console.WriteLine($"- {GroceryCategory}: {foodName}");
+                        }
+                    }
+                    else
+                    {
+                    Console.WriteLine("Error");  
+                    }     
+                }
+                Console.WriteLine("View the mealplan for date range? yes/no: ");
+                string reportdate = Console.ReadLine().Trim().ToLower();
+                if (reportdate=="yes")
+                {
+                    Console.WriteLine("Enter startdate (yyyy-MM-dd):");
+                    string startDateinput = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter enddate (yyyy-MM-dd):");
+                    string endDateinput = Console.ReadLine().Trim();
+
+                    if(DateTime.TryParse(startDateinput, out DateTime startDate) &&
+                        DateTime.TryParse(endDateinput, out DateTime endDate))
+                    {   
+                        //DateTime startDate = DateTime.Today;
+                        var meals = LoadMealPlanList();
+                        var selectedmeals = new List<MealPlan>();
+                        foreach(var meal in meals)
+                        {
+                            if (meal.Date.Date >= startDate && meal.Date.Date <= endDate.Date)
+                            {
+                            selectedmeals.Add(meal);
+                            }
+                        }
+                        selectedmeals.Sort((meal1,meal2)=>
+                        {
+                            int dateComparison= meal1.Date.CompareTo(meal2.Date);
+                            if(dateComparison == 0)
+                            {
+                                return meal1.MealTime.CompareTo(meal2.MealTime);
+                            }
+                            return dateComparison;
+                        });
+                        if (selectedmeals.Count >0)
+                        {
+                            Console.WriteLine($"meal plan from {startDate:yyyy-MM-dd} to {endDate:yyyy-MM-dd}");
+                            foreach (var meal in selectedmeals)
+                            {
+                                Console.WriteLine($"- {meal.Date:yyyy-MM-dd} ({meal.MealTime}): {meal.RecipeName.RecipeName}");
+                            }
+                        } 
+                        else
+                        {
+                            Console.WriteLine("No mealplan found for selected range.");
+                        }
+                    }
+                    else
+                    {
+                            Console.WriteLine("Please check start date or end date entries for valid format.");
+                    }
+                } 
+            }    
+        }while(usercommand !="exit"); 
+    } 
 }
